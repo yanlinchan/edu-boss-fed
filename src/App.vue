9cb1bf4fbd890d6d9cd98e1d70a06cb0a618aa5f
Yanlin <ylcresister@gmail.com>
2022-02-18 14:11:20 +0800

    样式处理

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,35 @@
   <div id="app">
     <h1>拉勾教育</h1>
     <!-- 根路由出口 -->
-    <router-view/>
+    <router-view />
+    <p class="text">hello</p>
   </div>
 </template>
 
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'App',
+  data () {
+    return {
+      a: 1,
+      c: { b: 1 }
+    }
+  },
+
+  methods: {
+    test () {
+      if (this.a) return true
+      else return null
+    }
+  }
+})
+</script>
+
 <style lang="scss" scoped>
+
+.text {
+  color: $warning-color;
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+
+// 加载全局样式
+import './styles/index.scss'
 
 Vue.config.productionTip = false
 
+Vue.use(ElementUI)
 new Vue({
   router,
   store,
